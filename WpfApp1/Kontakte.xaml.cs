9d2a08a625af002ade21a173d35f2febaeb1791e
joel <joel.quisbert@netigo.de>
2018-12-01 15:36:04 +0100

    init

---
+++ WpfApp1/Kontakte.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para Kontakte.xaml
+    /// </summary>
+    public partial class Kontakte : Window
+    {
+        public Kontakte()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            ((Image)sender).Source = new BitmapImage(new Uri("/assets/MyDefaultImage.png", UriKind.Relative));
+        }
+    }
+}
